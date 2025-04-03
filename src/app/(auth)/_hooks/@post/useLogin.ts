a3c00ae847f8a2ddec407d44ad90bb70d7b56bfd
dzaky-pr <dzaky.pr.mulyono97@gmail.com>
2025-04-03 12:26:30 +0700

    feat(auth): integrate admin login page

--- src/app/(auth)/_hooks/@post/useLogin.ts
+++ src/app/(auth)/_hooks/@post/useLogin.ts
@@ -7,7 +7,7 @@ import { UserLoginResponse, LoginRequest } from "@/types/auth";
 import { toast } from "@/hooks/use-toast";
 import { setToken } from "@/lib/cookies";
 
-export const useLoginMutation = () => {
+export const useUserLoginMutation = () => {
   const {
     mutate: handleLogin,
     isSuccess,
@@ -26,6 +26,51 @@ export const useLoginMutation = () => {
       toast({
         title: "Login successful",
         description: "Please wait, redirect to your dashboard.",
+        variant: "default",
+      });
+      setToken(data.token);
+    },
+    onError: (error: AxiosError<ApiError>) => {
+      if (error.response?.data?.message) {
+        const code = error.response?.data?.code || "";
+
+        toast({
+          title: `Login failed - ${code}`,
+          description: error.response.data.message,
+          variant: "destructive",
+        });
+      } else {
+        toast({
+          title: "Something went wrong",
+          description: "Sorry, please try again later.",
+          variant: "destructive",
+        });
+      }
+    },
+  });
+
+  return { handleLogin, isPending, isSuccess };
+};
+
+export const useAdminLoginMutation = () => {
+  const {
+    mutate: handleLogin,
+    isSuccess,
+    isPending,
+  } = useMutation<UserLoginResponse, AxiosError<ApiError>, LoginRequest>({
+    mutationFn: async (data) => {
+      const res = await api.post<ApiResponse<UserLoginResponse>>(
+        "/admin/auth/login",
+        data,
+      );
+
+      return res.data.data;
+    },
+
+    onSuccess: (data) => {
+      toast({
+        title: "Login successful",
+        description: "Please wait, redirect to your admin dashboard.",
         variant: "default",
       });
       setToken(data.token);
