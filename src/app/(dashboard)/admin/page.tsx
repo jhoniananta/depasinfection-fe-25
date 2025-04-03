48689d7ca081fc90903087f42d3ce556cdf1e3fb
Jhoni Ananta Sitepu <93531492+jhoniananta@users.noreply.github.com>
2025-04-03 12:59:12 +0700

    feat(dashboard): add layout dashboard user and admin (#36)

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,3 +1,11 @@
+import Sidebar from "@/layouts/SidebarAdmin";
+
 export default function AdminDashboardPage() {
-  return <div>AdminDashboardPage</div>;
+  return (
+    <>
+      <Sidebar title="Dashboard">
+        <section className="min-h-screen w-full">Test</section>
+      </Sidebar>
+    </>
+  );
 }
